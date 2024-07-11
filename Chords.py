55e33675ebb5635da35a6c8a0297a4a4c1dbff3e
sienheiser <emmanueldcosta97@gmail.com>
2024-07-11 20:24:14 +0200

    Made a chord class that can construct any chord.

--- Chords.py
+++ Chords.py
@@ -1,18 +1,37 @@
+import random
 import numpy as np
-notes = [i for i in range(13)]
-class MajorTriad:
-    def __init__(self,root_note:int):
-        self.triad = np.array([root_note,root_note+4,root_note+7])
-        self.fst_inversion = np.array([root_note+4,root_note+7,root_note+12])
-        self.snd_inversion = np.array([root_note+7,root_note+12,root_note+16])
 
-    def __repr__(self):
-        return f"{self.triad}"
+class Chord:
+    '''
+        This class builds chord using the root_note and pattern list.
+        The pattern list contains the number of semitones from the root
+        each additional key in a chord is. Example the major pattern is
+        [4,7]. A minor pattern is [3,7].
 
-class MinorTriad:
-    def __init(self,root_note:int):
-        self.triad = np.array([root_note,root_note+3,root_note+7])
-        self.fst_inversion = np.array([root_note+3,root_note+7,root_note+12])
-        self.snd_inversion = np.array([root_note+7,root_note+12,root_note+15])
+        This class also builds all the possible inversions of the chord.
+        '''
+    def __init__(self,root_note:int,pattern:list):
+        self.root_chord = []
+        self.root_chord.append(root_note)
+
+        for semitones in pattern:
+            self.root_chord.append(root_note+semitones)
+
+        self.variations = [np.array(self.root_chord)]
+
+        inversion = np.array(self.root_chord)
+        for i in range(len(pattern)):
+            inversion[0] = inversion[0] + 12
+            inversion = np.roll(inversion,-1)
+            self.variations.append(inversion)
+
+    def rng_variation(self) -> np.array:
+        return random.choice(self.variations)
+
+
+
+
+    
+
 
 
